e0a092bc4504d09bdc6b3b98386f6480539f1c2f
fafa194 <fafa194@hotmail.com>
2023-04-02 19:55:00 +0200

    Na razie tylko baza danych

---
+++ wojewodztwa.py
@@ -0,0 +1,14 @@
+
+
+rmf = {"kujawsko-pomorskie","dolnośląskie", "lubuskie", "łódzkie","mazowieckie","opolskie","podkarpackie","podlaskie","pomorskie","śląskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+zet = {"kujawsko-pomorskie","dolnośląskie", "lubelskie", "lubuskie", "łódzkie","podkarpackie","podlaskie","pomorskie","śląskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+eska = {"kujawsko-pomorskie","dolnośląskie", "lubelskie", "lubuskie", "łódzkie","małopolskie","mazowieckie","opolskie","podkarpackie","podlaskie","pomorskie","śląskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+vox = {"lubelskie", "lubuskie", "łódzkie","małopolskie","mazowieckie","opolskie","pomorskie","śląskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+disco = {"kujawsko-pomorskie","dolnośląskie", "lubelskie", "lubuskie", "łódzkie","małopolskie","mazowieckie","opolskie","podkarpackie","podlaskie"}
+wnet = {"kujawsko-pomorskie","dolnośląskie","mazowieckie","opolskie","podkarpackie","podlaskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+pollo = {"kujawsko-pomorskie","dolnośląskie", "lubelskie", "lubuskie", "łódzkie","małopolskie","mazowieckie","opolskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+sepi = {"opolskie","podkarpackie","podlaskie","pomorskie","śląskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"}
+
+radio_array_list = [rmf,zet,eska,vox,disco,wnet,pollo,sepi]
+for wojewodztwa in radio_array_list:
+    print(wojewodztwa)
