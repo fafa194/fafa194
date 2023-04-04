89dffb439b954a20d4141f212af3a69a27ad7e6e
fafa194 <fafa194@hotmail.com>
2023-04-04 21:42:03 +0200

    Poprawione obliczenia, poprawiono przejrzystość

--- wojewodztwa.py
+++ wojewodztwa.py
@@ -19,9 +19,11 @@ for i, each_radio in enumerate(radio_array_list):
     each_radio_len = len(each_radio) # Zasięg zmierzony w liczbie
     print(radio_array_list_strings[i], each_radio_len)
     # Kazdy zasięg porównuję z każdym
-    for j in range(len(radio_array_list_strings)):
-        radio_minus_radio = set(each_radio) - set(radio_array_list[j]) # Które zasięgi nie pokrywają się
+    for j in range(len(radio_array_list)):
+        radio_minus_radio = each_radio - radio_array_list[j] # Które zasięgi nie pokrywają się
+        radio_intersection = each_radio & radio_array_list[j] # Które zasięgi się pokrywają
         if len(radio_minus_radio) == 0:
             print("OK")
         else:
-            print(radio_minus_radio, radio_array_list_strings[i], " I " , radio_array_list_strings[j])
+            print(radio_array_list_strings[j], "nie pokrywa się z", radio_array_list_strings[i], "w obszarach", radio_minus_radio)
+    print()
